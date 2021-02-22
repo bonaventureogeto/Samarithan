78e10d6e08482d136a0bf59f6b7277ba9de674d8
Bonaventure Ogeto <bonaogeto1@gmail.com>
2021-02-22 17:51:40 +0300

    Registered subject app models to the admin

--- subject/admin.py
+++ subject/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import Subject, Candidate, PollingStation, SubLocation, Location, Ward, Constituency, SubCounty, County, PoliticalParty
 
-admin.site.register(Subject)
-admin.site.register(Candidate)
-admin.site.register(PollingStation)
-admin.site.register(SubLocation)
-admin.site.register(Location)
-admin.site.register(Ward)
-admin.site.register(Constituency)
-admin.site.register(SubCounty)
+
 admin.site.register(County)
+admin.site.register(SubCounty)
+admin.site.register(Constituency)
+admin.site.register(Ward)
+admin.site.register(Location)
+admin.site.register(SubLocation)
+admin.site.register(PollingStation)
+admin.site.register(Candidate)
 admin.site.register(PoliticalParty)
+admin.site.register(Subject)
